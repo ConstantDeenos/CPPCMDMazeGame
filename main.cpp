3dc2e9fcdb6f9cba2b32f39e9bda8c4643bf8f4a
Constant Deenos <konstantinos.prasinos@gmail.com>
2021-06-15 17:23:55 +0300

    Add menu to main.cpp Add losing message Add intelligence to player for com mode

--- main.cpp
+++ main.cpp
@@ -12,22 +12,88 @@
 using namespace std;
 
 int main(){
+    int gameQuit = 0;
+    int selection = 1;
+    int input;
+    string gameType;
     //Initialise Screen, set up memory, clear the screen and some settings
     initscr();
     raw();
     keypad(stdscr, TRUE);
     noecho();
-    string mapString = "map.txt";
-    Engine engine;
-    vector<string> map = engine.getMap();
+    curs_set(0);
 
-    engine.printMap();
     do{
-        engine.nextRound();
-    } while(!(engine.getGameState() == "Win" || engine.getGameState() == "End" || engine.getGameState() == "Lost"));
+        do{
+            clear();
+            if (selection == 1){
+                printw("> ");
+            }
+            printw("Play -Player vs Computer-\n");
+            if (selection == 2){
+                printw("> ");
+            }
+            printw("Play -Computer vs Computer-\n");
+            if (selection == 3){
+                printw("> ");
+            }
+            printw("Quit\n");
+            refresh();
+            input = getch();
+            switch (input)
+            {
+            case KEY_UP:
+                if (selection > 1){
+                    selection--;
+                }
+                break;
+            case KEY_DOWN:
+                if (selection < 3){
+                    selection++;
+                }
+                break;
+            case 27:
+                selection = 3;
+                break;
+            default:
+                break;
+            }
+        } while (input != 10 && input != 27);
 
-    endwin();
-    cout<<engine.getGameState();
+        switch (selection)
+        {
+        case 1:{
+            gameType = "Player";
+
+            Engine engine;
+            engine.printMap();
+            do{
+                engine.nextRound();
+            }while(!(engine.getGameState() == "Win" || engine.getGameState() == "End" || engine.getGameState() == "Lost"));
+            }
+            break;
+        case 2:
+            gameType = "COM";
+            break;
+        case 3:
+            gameType = "NONE";
+            gameQuit = 1;
+            break;
+        default:
+            break;
+        }
+    } while (gameQuit == 0);
+
+    // string mapString = "map.txt";
+    // Engine engine;
+    // vector<string> map = engine.getMap();
+
+    // engine.printMap();
+    // do{
+    //     engine.nextRound();
+    // } while(!(engine.getGameState() == "Win" || engine.getGameState() == "End" || engine.getGameState() == "Lost"));
+
+    // endwin();
 
     return 0;
 }
